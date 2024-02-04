e168cad661127afb3d0aa81b168a8396faf6769a
RohitChanda <rohit.chanda.93@gmail.com>
2024-02-04 13:18:31 +0530

    intial commit into develop

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,29 +1,17 @@
 import React, { useEffect, useRef } from "react";
-import {
-  YOUTUBE_API,
-  getSearchResults,
-  getYoutubeVideosList,
-} from "../utils/youtubeApi";
-import { savedVideos } from "../utils/constant";
-import { useState } from "react";
 import VideoCard from "./VideoCard";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  // addVideosIntoList,
   fetchVideos,
-  // updateWholeVideoList,
   fetchMoreVideos
 } from "../store/slice/videoListSlice";
 import useDebounce from "../hooks/useDebounce";
 
 const VideoContainer = () => {
   let [searchParams] = useSearchParams();
-  const searchKey = searchParams.get("search_query");
-
   const videoListState =  useSelector((state) => state.videosList)
   const videos = useSelector((state) => state.videosList.videos);
-  // const nextPageToken = useSelector((state) => state.videosList.nextPageToken)
   const dispatch = useDispatch();
 
   const nextPageTokenRef = useRef(null);
@@ -68,7 +56,6 @@ const VideoContainer = () => {
   return (
     <div className="flex flex-wrap">
       {
-        // <VideoCard key={videos[0]?.id || 0} info={videos[0]} />
         videos.map((video, index) => (
           <VideoCard key={index} info={video} />
         ))
--- src/utils/youtubeApi.js
+++ src/utils/youtubeApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const GOOGLE_API_KEY = 'AIzaSyCG2AZ2DRpTWWpFI4sK2RVab19zdO6sY7Y';
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=8&pageToken=CAgQAA&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
@@ -17,14 +18,5 @@ export const getYoutubeVideosList = async(pageToken='') => {
     let apiKey = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=8&pageToken=${pageToken}&regionCode=IN&key=${GOOGLE_API_KEY}`;
     const data = await fetch(apiKey);
     const json = await data.json();
-    // const data = await axios.get(apiKey)
     return json;
 }
-
-
-
-// GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&pageToken=CAgQAA&q=football&regionCode=IN&key=[YOUR_API_KEY] HTTP/1.1
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
-
